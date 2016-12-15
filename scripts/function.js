edd173006c10eea1d9b061aee1127ce96f1353d3
nezharen <nezharen1995@gmail.com>
2016-12-15 15:22:16 +0800

    layout

--- scripts/function.js
+++ scripts/function.js
@@ -100,8 +100,59 @@ function sugiyama()
 	}
 }
 
+function topoSort()
+{
+	topoLayout = [];
+	orderNum = [];
+	for (var i = 0; i < activityNum; i++)
+		topoLayout.push({"level": 0, "order": i});
+	orderNum.push(activityNum);
+	levelNum = 1;
+	maxOrder = activityNum;
+}
+
+function paint()
+{
+	var goldenRation = 1.618;
+	rectWidth = svgWidth / (2 * maxOrder + 1);
+	rectHeight = svgHeight / (2 * levelNum + 1);
+	if (rectHeight * goldenRation < rectWidth)
+		rectWidth = rectHeight * goldenRation;
+	else
+		rectHeight = rectWidth / goldenRation;
+	layout = [];
+	for (var i = 0; i < activityNum; i++)
+	{
+		var spaceWidth = (svgWidth - rectWidth * orderNum[topoLayout[i]["level"]]) / (orderNum[topoLayout[i]["level"]] + 1);
+		var spaceHeight = (svgHeight - rectHeight * levelNum) / (levelNum + 1);
+		layout.push({"x": spaceWidth * (topoLayout[i]["order"] + 1) + rectWidth * topoLayout[i]["order"], "y": spaceHeight * (topoLayout[i]["level"] + 1) + rectHeight * topoLayout[i]["level"]});
+
+	}
+	var activityContainers = svg.selectAll("g")
+		.data(layout)
+		.enter()
+		.append("g")
+		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	activityContainers.append("rect")
+		.attr("width", rectWidth)
+		.attr("height", rectHeight)
+		.attr("stroke", "black")
+		.attr("stroke-width", 2)
+		.attr("rx", 5)
+		.attr("fill", "#4d97d6");
+	activityContainers.append("text")
+		.attr("x", 10)
+		.attr("y", rectHeight / 2)
+		.attr("fill", "white")
+		.attr("font-weight", "bold")
+		.text(function(d, i) { return graph["activity_name"][i]; });
+}
+
 function init()
 {
+	svgWidth = document.documentElement.clientWidth - 100;
+	svgHeight = document.documentElement.clientHeight - 100;
+	svg = d3.select("body").append("svg").attr("width", svgWidth).attr("height", svgHeight);
 	d3.json("data/graphNet.json", function (error, data) {
 			graph = data;
 			activityNum = graph["activity_name"].length;
@@ -109,5 +160,9 @@ function init()
 			edgeType = "total_duration_edge";
 			threshold = 0;
 			sugiyama();
+			topoSort();
+			paint();
 			});
 }
+
+init();
