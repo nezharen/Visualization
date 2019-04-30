31f490fd3bbddd3d3c14300d56e8b0acb0d29ce5
children <children19930928@gmail.com>
2019-04-30 16:55:19 +0800

    build script for windows

---
+++ backend/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cur_dir=$(pwd)
+echo $cur_dir
+
+javac -encoding UTF-8 -d "classes" -classpath ".\\lib\\json-20160810.jar;.\\lib\\opencsv-3.3.jar;.;" .\\src\\mining\\*.java
+
+java -classpath ".\\lib\\json-20160810.jar;.\\lib\\opencsv-3.3.jar;.\\classes;" mining.Main
+
+read -p "Finished! Press any key to continue..." var
+
